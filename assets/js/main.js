26adee433fa8ba39cac2ef962e6ae13215021b79
unknown <okal.ihor@gmail.com>
2017-06-11 18:44:26 +0300

    Main page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,127 @@
 $(document).ready(function () {
-	// body...
+
+	function initMap() {
+    // Create the map with no initial style specified.
+    // It therefore has default styling.
+    var kords = {lat: 50.415178, lng: 30.530963};
+
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: kords,
+      zoom: 13,
+      mapTypeControl: false
+    });
+
+    map.setOptions({styles: styles['silver']});
+
+    var infowindow = new google.maps.InfoWindow({
+      content: '<div class="tolltip"><span>Отель Базис</span><p>г. Киев, Печерск, ул. Кудри 7</p></div>'
+    });
+
+
+
+    var marker = new google.maps.Marker({
+      position: kords,
+      map: map,
+      title: 'Apart Hotel',
+      icon: null
+    });
+
+    marker.addListener('click', function() {
+      infowindow.open(map, marker);
+    });
+  }
+
+  var styles = {
+    default: null,
+    silver: [
+      {
+        elementType: 'geometry',
+        stylers: [{color: '#f5f5f5'}]
+      },
+      {
+        elementType: 'labels.icon',
+        stylers: [{visibility: 'off'}]
+      },
+      {
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#616161'}]
+      },
+      {
+        elementType: 'labels.text.stroke',
+        stylers: [{color: '#f5f5f5'}]
+      },
+      {
+        featureType: 'administrative.land_parcel',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#bdbdbd'}]
+      },
+      {
+        featureType: 'poi',
+        elementType: 'geometry',
+        stylers: [{color: '#eeeeee'}]
+      },
+      {
+        featureType: 'poi',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#757575'}]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'geometry',
+        stylers: [{color: '#e5e5e5'}]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#9e9e9e'}]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry',
+        stylers: [{color: '#ffffff'}]
+      },
+      {
+        featureType: 'road.arterial',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#757575'}]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry',
+        stylers: [{color: '#dadada'}]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#616161'}]
+      },
+      {
+        featureType: 'road.local',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#9e9e9e'}]
+      },
+      {
+        featureType: 'transit.line',
+        elementType: 'geometry',
+        stylers: [{color: '#e5e5e5'}]
+      },
+      {
+        featureType: 'transit.station',
+        elementType: 'geometry',
+        stylers: [{color: '#eeeeee'}]
+      },
+      {
+        featureType: 'water',
+        elementType: 'geometry',
+        stylers: [{color: '#c9c9c9'}]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#9e9e9e'}]
+      }
+    ]
+  };
+
+  initMap();
 });
