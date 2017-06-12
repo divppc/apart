6e5e03ac7207778d49d94129424ef3c78431d9d7
unknown <okal.ihor@gmail.com>
2017-06-12 22:03:55 +0300

    html part done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,127 +1,142 @@
-$(document).ready(function () {
+$(document).ready(function() {    
 
-	function initMap() {
-    // Create the map with no initial style specified.
-    // It therefore has default styling.
-    var kords = {lat: 50.415178, lng: 30.530963};
+  //mobile toggle function start
 
-    map = new google.maps.Map(document.getElementById('map'), {
-      center: kords,
-      zoom: 13,
-      mapTypeControl: false
+  $(".mobile-menu").on("click", function () {
+    var e = $("nav");
+    mobileToggle(e);
+  });
+
+  function mobileToggle(e) {
+    if ($(e).hasClass("active") == true) {
+      $(e).animate({maxHeight: "0"}, 100).removeClass("active");
+    } else {
+      $(e).animate({maxHeight: "100vh"}, 100).addClass("active");
+    }
+  };  
+  //mobile toggle function end
+
+  //choose language
+  $(".select-language").on("click", function() {
+    $(this).toggleClass("active");
+  });
+
+  //scroll to top
+  $('.to-top').click(function(e) {
+    e.preventDefault();
+    $("html, body").animate({
+        scrollTop: $("header").offset().top
+    }, {
+        duration: 1500
     });
+  });
 
-    map.setOptions({styles: styles['silver']});
-
-    var infowindow = new google.maps.InfoWindow({
-      content: '<div class="tolltip"><span>Отель Базис</span><p>г. Киев, Печерск, ул. Кудри 7</p></div>'
-    });
-
-
-
-    var marker = new google.maps.Marker({
-      position: kords,
-      map: map,
-      title: 'Apart Hotel',
-      icon: null
-    });
-
-    marker.addListener('click', function() {
-      infowindow.open(map, marker);
-    });
-  }
-
-  var styles = {
-    default: null,
-    silver: [
+  //header slider
+  $(".header-slider .slides").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    prevArrow: ".slider-left",
+    nextArrow: ".slider-right",
+    mobileFirst: true,
+    responsive: [
       {
-        elementType: 'geometry',
-        stylers: [{color: '#f5f5f5'}]
-      },
-      {
-        elementType: 'labels.icon',
-        stylers: [{visibility: 'off'}]
-      },
-      {
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#616161'}]
-      },
-      {
-        elementType: 'labels.text.stroke',
-        stylers: [{color: '#f5f5f5'}]
-      },
-      {
-        featureType: 'administrative.land_parcel',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#bdbdbd'}]
-      },
-      {
-        featureType: 'poi',
-        elementType: 'geometry',
-        stylers: [{color: '#eeeeee'}]
-      },
-      {
-        featureType: 'poi',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#757575'}]
-      },
-      {
-        featureType: 'poi.park',
-        elementType: 'geometry',
-        stylers: [{color: '#e5e5e5'}]
-      },
-      {
-        featureType: 'poi.park',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#9e9e9e'}]
-      },
-      {
-        featureType: 'road',
-        elementType: 'geometry',
-        stylers: [{color: '#ffffff'}]
-      },
-      {
-        featureType: 'road.arterial',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#757575'}]
-      },
-      {
-        featureType: 'road.highway',
-        elementType: 'geometry',
-        stylers: [{color: '#dadada'}]
-      },
-      {
-        featureType: 'road.highway',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#616161'}]
-      },
-      {
-        featureType: 'road.local',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#9e9e9e'}]
-      },
-      {
-        featureType: 'transit.line',
-        elementType: 'geometry',
-        stylers: [{color: '#e5e5e5'}]
-      },
-      {
-        featureType: 'transit.station',
-        elementType: 'geometry',
-        stylers: [{color: '#eeeeee'}]
-      },
-      {
-        featureType: 'water',
-        elementType: 'geometry',
-        stylers: [{color: '#c9c9c9'}]
-      },
-      {
-        featureType: 'water',
-        elementType: 'labels.text.fill',
-        stylers: [{color: '#9e9e9e'}]
+        breakpoint: 768,
+        settings: {
+          dots: true,
+          dotsClass: 'dots',
+        }
       }
     ]
+  });
+
+  //response slider
+  $(".response-container").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true,
+    arrows: false,
+    mobileFirst: true,
+    dots: true,
+    dotsClass: 'dots',
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      },
+
+      {
+        breakpoint: 390,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      },
+    ]
+  });
+
+  //gallary inside slider
+
+  $(".thumb a").on("click", function (e) {
+    e.preventDefault();
+  });
+
+  function photoSlider() {
+
+    $('.main-photo').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      asNavFor: '.thumbs'
+    });
+
+    $('.thumbs').slick({
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      asNavFor: '.main-photo',
+      prevArrow: ".slider-left",
+      nextArrow: ".slider-right",
+      focusOnSelect: true
+    });
   };
 
-  initMap();
-});
+  photoSlider();
+  //product photos slider end
+
+  $('.main-photo').on('afterChange', function(event, slick, currentSlide, nextSlide){
+    $(".fancybox").fancybox({
+       helpers : {
+            overlay : {
+                css : {
+                    'background' : 'rgba(0, 0, 0, 0.7)'
+                }
+            }
+        }
+    });
+  });
+
+  //zoom picture
+  $(".fancybox").fancybox({
+     helpers : {
+          overlay : {
+              css : {
+                  'background' : 'rgba(0, 0, 0, 0.7)'
+              }
+          }
+      }
+  });
+
+  //booking date dropper
+  $(".dates").dateDropper({
+    lang: 'ru',
+    animate: false,
+    dropPrimaryColor: '#b5876d',
+    minYear: '2016',
+    format: 'd/m/Y'
+  });
+
+})
---
+++ assets/js/map.js
@@ -0,0 +1,118 @@
+$(document).ready(function () {
+
+	function initMap() {
+    // Create the map with no initial style specified.
+    // It therefore has default styling.
+    var kords = {lat: 50.415178, lng: 30.530963};
+
+    map = new google.maps.Map(document.getElementById('map'), {
+      center: kords,
+      zoom: 13,
+      mapTypeControl: false,
+      scrollwheel: false,
+    });
+
+    map.setOptions({styles: styles['silver']});
+
+    var marker = new google.maps.Marker({
+      position: kords,
+      map: map,
+      title: 'Apart Hotel',
+      icon: "/assets/images/ui/marker.png"
+    });
+  }
+
+  var styles = {
+    default: null,
+    silver: [
+      {
+        elementType: 'geometry',
+        stylers: [{color: '#f5f5f5'}]
+      },
+      {
+        elementType: 'labels.icon',
+        stylers: [{visibility: 'off'}]
+      },
+      {
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#616161'}]
+      },
+      {
+        elementType: 'labels.text.stroke',
+        stylers: [{color: '#f5f5f5'}]
+      },
+      {
+        featureType: 'administrative.land_parcel',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#bdbdbd'}]
+      },
+      {
+        featureType: 'poi',
+        elementType: 'geometry',
+        stylers: [{color: '#eeeeee'}]
+      },
+      {
+        featureType: 'poi',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#757575'}]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'geometry',
+        stylers: [{color: '#e5e5e5'}]
+      },
+      {
+        featureType: 'poi.park',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#9e9e9e'}]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry',
+        stylers: [{color: '#ffffff'}]
+      },
+      {
+        featureType: 'road.arterial',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#757575'}]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry',
+        stylers: [{color: '#dadada'}]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#616161'}]
+      },
+      {
+        featureType: 'road.local',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#9e9e9e'}]
+      },
+      {
+        featureType: 'transit.line',
+        elementType: 'geometry',
+        stylers: [{color: '#e5e5e5'}]
+      },
+      {
+        featureType: 'transit.station',
+        elementType: 'geometry',
+        stylers: [{color: '#eeeeee'}]
+      },
+      {
+        featureType: 'water',
+        elementType: 'geometry',
+        stylers: [{color: '#c9c9c9'}]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#9e9e9e'}]
+      }
+    ]
+  };
+
+  initMap();
+});
